3c604c621921161ceb5e20ef2b9587ead5012acb
xingstar007 <xingstar-006@163.com>
2015-01-24 23:47:12 +0800

    add template

---
+++ application/views/layouts/default.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php echo $template['partials']['header']; ?>
+	</head>
+	<body>
+		<h1><?php echo $template['title']; ?></h1>
+		<?php echo $template['body']; ?>
+	</body>
+	<footer>
+		<?php echo $template['partials']['footer']; ?>
+	</footer>
+</html>
