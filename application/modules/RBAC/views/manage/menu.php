1a26e8f2a6b68939db124cdfbdd1f55436e668ee
xingstar007 <xingstar-006@163.com>
2015-02-05 18:16:15 +0800

    RBAC

--- application/modules/RBAC/views/manage/menu.php
+++ application/modules/RBAC/views/manage/menu.php
@@ -6,17 +6,23 @@
 foreach($menu as $mn){
 	$nochild = "<span class='glyphicon glyphicon-align-left'></span> " ;
 	$havechild = "<span class='glyphicon glyphicon-link'></span> " ;
-	echo '<table class="table well">';
+	echo '<div class="table-responsive" >';
+	echo '<table class="table table-responsive">';
 	echo '<tr>
 		  	<td><b>'.($mn["self"]->node_id?$havechild:$nochild).$mn["self"]->title.'</b></td>
 		  	<td>'.($mn["self"]->memo?$mn["self"]->memo.$mn["self"]->dcf:"未挂接").'</td>
 		  	<td>'.$mn["self"]->sort.'</td>
 		  	<td>'.($mn["self"]->status==1?"显示":"隐藏").'</td>
-		  	<td><div class="btn-group  btn-group-xs pull-right">
-				  <a class="btn btn-default" href="'.site_url("RBAC/menu/edit/".$mn["self"]->id."/1/NULL").'">修改</a>
-				  <a class="btn btn-danger" href="'.site_url("RBAC/menu/delete/".$mn["self"]->id).'">删除</a>
-				  <a class="btn btn-success" href="'.site_url("RBAC/menu/add/".$mn["self"]->id."/1/".$mn["self"]->id).'">新增子菜单</a>
-				</div>
+		  	<td class="table-action pull-left">
+				<a href="'.site_url("RBAC/menu/edit/".$mn["self"]->id."/1/NULL").'">
+					<i class="fa fa-pencil"></i>
+				</a>
+				<a href="'.site_url("RBAC/menu/delete/".$mn["self"]->id).'">
+					<i class="fa fa-trash-o"></i>	
+				</a>
+				<a href="'.site_url("RBAC/menu/add/".$mn["self"]->id."/1/".$mn["self"]->id).'">
+					<i class="fa fa-plus"></i>		
+				</a>
 			</td>
 		  </tr>';
 	if(isset($mn["child"])){
@@ -26,34 +32,19 @@ foreach($menu as $mn){
 			  	<td>'.($cmn["self"]->memo?$cmn["self"]->memo.$cmn["self"]->dcf:"未挂接").'</td>
 			  	<td>'.$cmn["self"]->sort.'</td>
 			  	<td>'.($cmn["self"]->status==1?"显示":"隐藏").'</td>
-			  	<td><div class="btn-group  btn-group-xs pull-right">
-					  <a class="btn btn-default" href="'.site_url("RBAC/menu/edit/".$cmn["self"]->id."/2/".$mn["self"]->id).'">修改</a>
-					  <a class="btn btn-danger" href="'.site_url("RBAC/menu/delete/".$cmn["self"]->id).'">删除</a>
-					  <a class="btn btn-success" href="'.site_url("RBAC/menu/add/".$cmn["self"]->id."/2/".$cmn["self"]->id).'">新增子菜单</a>
-					</div>
+			  	<td class="table-action pull-left">
+					  <a href="'.site_url("RBAC/menu/edit/".$cmn["self"]->id."/2/".$mn["self"]->id).'">
+							<i class="fa fa-pencil"></i>
+					  </a>
+					  <a href="'.site_url("RBAC/menu/delete/".$cmn["self"]->id).'">
+							<i class="fa fa-trash-o"></i>	
+					  </a>
 				</td>
 			  </tr>';
-			
-			if(isset($cmn["child"])){
-				foreach($cmn["child"] as $gcmn){
-					echo '<tr>
-						  	<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  					<b>'.($gcmn["self"]->node_id?$havechild:"").$gcmn["self"]->title.'</b></td>
-						  	<td>'.($gcmn["self"]->memo?$gcmn["self"]->memo.$gcmn["self"]->dcf:"未挂接").'</td>
-			  				<td>'.$gcmn["self"]->sort.'</td>
-						  	<td>'.($gcmn["self"]->status==1?"显示":"隐藏").'</td>
-						  	<td><div class="btn-group  btn-group-xs pull-right">
-								  <a class="btn btn-default" href="'.site_url("RBAC/menu/edit/".$gcmn["self"]->id."/3/".$cmn["self"]->id).'">修改</a>
-								  <a class="btn btn-danger" href="'.site_url("RBAC/menu/delete/".$gcmn["self"]->id).'">删除</a>
-								</div>
-							</td>
-						  </tr>';
-				}
-			}
 		}
 	}
 	echo '</table>';
+	echo '</div>';
 }
 ?>
 <hr/>
